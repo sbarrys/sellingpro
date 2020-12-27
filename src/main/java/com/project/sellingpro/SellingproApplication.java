dd6760cfc465e03e81b2474b49cc0cc1d1dcaaf5
Kim Tae Yun <rlaxodbs2015@gmail.com>
2020-12-27 17:28:31 +0900

    Init : Project initialize

---
+++ src/main/java/com/project/sellingpro/SellingproApplication.java
@@ -0,0 +1,13 @@
+package com.project.sellingpro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SellingproApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SellingproApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/sellingpro/SellingproApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.sellingpro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SellingproApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
