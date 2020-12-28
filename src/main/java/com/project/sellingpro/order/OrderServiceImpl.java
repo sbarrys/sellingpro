0780a54640bce754670efccff0aec29e43973b00
Kim Tae Yun <rlaxodbs2015@gmail.com>
2020-12-28 18:02:27 +0900

    Fix : DIP 위반 없애기-1

--- src/main/java/com/project/sellingpro/order/OrderServiceImpl.java
+++ src/main/java/com/project/sellingpro/order/OrderServiceImpl.java
@@ -1,8 +1,6 @@
 package com.project.sellingpro.order;
 
 import com.project.sellingpro.discount.DiscountPolicy;
-import com.project.sellingpro.discount.FixDiscountPolicy;
-import com.project.sellingpro.discount.RateDiscountPolicy;
 import com.project.sellingpro.member.Member;
 import com.project.sellingpro.member.MemberRepository;
 import com.project.sellingpro.member.MemoryMemberRepository;
@@ -10,10 +8,10 @@ import com.project.sellingpro.member.MemoryMemberRepository;
 public class OrderServiceImpl implements OrderService{
 
     private final MemberRepository memberRepository = new MemoryMemberRepository();
-    //할인 정책을 변경하려면 클라이언트인 OrderServiceImple 의 코드를 고쳐야한다. 우리는 인터페이스만 의존하는것이 아닌, 구현체도 의존하고 있던것. (DIP 위반)
-    // 그래서 "수정"하는 순간 OCP가 위반된다.
-    //private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
-    private final DiscountPolicy discountPolicy = new RateDiscountPolicy();
+    //인터페이스만 의존하도록 싹 지웠다.
+    //그러면 NullPoint 인데 어떻게 하면 될까?
+    //해결방안 : Spring 이 객체를 대신 생성해서 대신 주입시켜주는 방식으로 해결한다..
+    private DiscountPolicy discountPolicy;
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
