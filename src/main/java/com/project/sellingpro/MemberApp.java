95b787edd4e49650abf1237373632a49ec5da8f5
Kim Tae Yun <rlaxodbs2015@gmail.com>
2020-12-28 14:44:27 +0900

    Add : JAVA main code

---
+++ src/main/java/com/project/sellingpro/MemberApp.java
@@ -0,0 +1,20 @@
+package com.project.sellingpro;
+
+import com.project.sellingpro.member.Grade;
+import com.project.sellingpro.member.Member;
+import com.project.sellingpro.member.MemberService;
+import com.project.sellingpro.member.MemberServiceImpl;
+
+public class MemberApp {
+
+    public static void main(String[] args) {
+        MemberService memberService = new MemberServiceImpl();
+        Member member = new Member(1L,"memberA", Grade.VIP);
+        memberService.join(member);
+
+        Member findmember = memberService.findMember(1L);
+
+        System.out.println("new member  = " + member);
+        System.out.println("find member  = " + findmember);
+    }
+}
