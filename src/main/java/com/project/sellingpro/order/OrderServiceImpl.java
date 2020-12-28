b4201622046176f315eef293ce96bedb95ef26c6
Kim Tae Yun <rlaxodbs2015@gmail.com>
2020-12-28 17:54:55 +0900

    Warn : DIP 위반 (order -> discountPolicy 인터페이스 , 구현체) 즉, 수정시 OCP 위반

--- src/main/java/com/project/sellingpro/order/OrderServiceImpl.java
+++ src/main/java/com/project/sellingpro/order/OrderServiceImpl.java
@@ -2,6 +2,7 @@ package com.project.sellingpro.order;
 
 import com.project.sellingpro.discount.DiscountPolicy;
 import com.project.sellingpro.discount.FixDiscountPolicy;
+import com.project.sellingpro.discount.RateDiscountPolicy;
 import com.project.sellingpro.member.Member;
 import com.project.sellingpro.member.MemberRepository;
 import com.project.sellingpro.member.MemoryMemberRepository;
@@ -9,7 +10,10 @@ import com.project.sellingpro.member.MemoryMemberRepository;
 public class OrderServiceImpl implements OrderService{
 
     private final MemberRepository memberRepository = new MemoryMemberRepository();
-    private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
+    //할인 정책을 변경하려면 클라이언트인 OrderServiceImple 의 코드를 고쳐야한다. 우리는 인터페이스만 의존하는것이 아닌, 구현체도 의존하고 있던것. (DIP 위반)
+    // 그래서 "수정"하는 순간 OCP가 위반된다.
+    //private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
+    private final DiscountPolicy discountPolicy = new RateDiscountPolicy();
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
