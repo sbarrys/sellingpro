bbdbb7317c5988655b75cf23f8d2a92be9d068b5
Kim Tae Yun <rlaxodbs2015@gmail.com>
2020-12-30 14:14:03 +0900

    Fix : Refactoring Appconfig  ( Extract Method )

--- src/main/java/com/project/sellingpro/AppConfig.java
+++ src/main/java/com/project/sellingpro/AppConfig.java
@@ -1,6 +1,8 @@
 package com.project.sellingpro;
 
+import com.project.sellingpro.discount.DiscountPolicy;
 import com.project.sellingpro.discount.FixDiscountPolicy;
+import com.project.sellingpro.member.MemberRepository;
 import com.project.sellingpro.member.MemberService;
 import com.project.sellingpro.member.MemberServiceImpl;
 import com.project.sellingpro.member.MemoryMemberRepository;
@@ -8,16 +10,28 @@ import com.project.sellingpro.order.OrderService;
 import com.project.sellingpro.order.OrderServiceImpl;
 
 public class AppConfig {
+
+    private MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
+    }
+
+    private DiscountPolicy discountPolicy() {
+        return new FixDiscountPolicy();
+    }
+
     //기존에는 MemberService 에서 직접 memberRepository들을 생성하고 했었다. 그것은 배우가 배우를 직접 초빙하는 일과 같다.
     //이제는  Appconfig에서 그런것들을 해주도록 하겠다.
     public MemberService memberService(){
         //MemeberService 의 MemeberRepository는 구현체를 만들지 않고 생성자로 넣어주었다.
         //그 생성자를 Appconfig가 만들어준다..
         //그래서 자동으로 아래에있는 memoryMemberRepository가 들어가게 된다.
-        return new MemberServiceImpl(new MemoryMemberRepository());
+        return new MemberServiceImpl(memberRepository());
     }
 
+
+
     public OrderService orderService(){
-        return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
+        return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
+
 }
