ea6e2125a3ba24881c19c9c55ded608c5d708198
Kim Tae Yun <rlaxodbs2015@gmail.com>
2021-01-05 23:54:21 +0900

    Fix : @RequiredArgsConstructor
    - final 붙은 변수들을 모두  변수로 사용한 생성자를 생성해준다. (롬복을 이용해서 의존성 주입 쉽게하기)

--- src/main/java/com/project/sellingpro/order/OrderServiceImpl.java
+++ src/main/java/com/project/sellingpro/order/OrderServiceImpl.java
@@ -3,16 +3,20 @@ package com.project.sellingpro.order;
 import com.project.sellingpro.discount.DiscountPolicy;
 import com.project.sellingpro.member.Member;
 import com.project.sellingpro.member.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
 
+@Component
+@RequiredArgsConstructor
 public class OrderServiceImpl implements OrderService{
     //이제 순수한 인터페이스에만 의존한다.
     private final MemberRepository memberRepository;
     private final DiscountPolicy discountPolicy;
     //Appconfig 가 실제 구현체를 탑재한 생성자를 만들어서  넣어준다.
-    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
-        this.memberRepository = memberRepository;
-        this.discountPolicy = discountPolicy;
-    }
+//    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
+//        this.memberRepository = memberRepository;
+//        this.discountPolicy = discountPolicy;
+//    }
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
